e83ca60ddc1c99642dfbe62dd6c40ef20f141030
jaeevass <rv.edu99cat@gmail.com>
2026-01-06 10:28:05 +0700

    up

--- codeigniter4-framework-e4d3702/app/Config/Routes.php
+++ codeigniter4-framework-e4d3702/app/Config/Routes.php
@@ -9,4 +9,4 @@ $routes->get('/', 'Home::index');
 $routes->get('test-db', 'TestDB::index');
 $routes->post('shipping', 'ShippingController::create');
 $routes->post('api/shipping/create', 'ShippingController::create');
-$routes->get('api/shipping/status/(:num)', 'ShippingController::status/$1');
+$routes->get('api/shipping/status/(:segment)', 'ShippingController::status/$1');
