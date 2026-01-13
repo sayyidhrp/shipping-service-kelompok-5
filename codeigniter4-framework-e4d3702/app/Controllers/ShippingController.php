2b25f84023f69f6dbc38aa58d23d8544ad225da4
sayyidhrp <sayyid.harahap@tm.idu.ac.id>
2026-01-13 11:48:04 +0700

    feat(api): validate order_id and handle not found case

--- codeigniter4-framework-e4d3702/app/Controllers/ShippingController.php
+++ codeigniter4-framework-e4d3702/app/Controllers/ShippingController.php
@@ -5,35 +5,38 @@ namespace App\Controllers;
 use CodeIgniter\RESTful\ResourceController;
 
 class ShippingController extends ResourceController
-{
+    {
     public function index()
     {
         return $this->respond([
             'message' => 'Shipping API is running'
         ]);
     }
-
+                                                                                                                                                                                                                                                                                                                                                                    
     public function status($order_id = null)
 {
     if (!$order_id) {
-        return $this->fail([
-            'error' => true,
+        return $this->respond([
+            'success' => false,
             'message' => 'Order ID tidak boleh kosong'
         ], 400);
     }
 
-    // simulasi order tidak ditemukan
     if ($order_id !== 'ORD123') {
-        return $this->fail([
-            'error' => true,
+        return $this->respond([
+            'success' => false,
             'message' => 'Order ID tidak ditemukan'
         ], 404);
     }
 
     return $this->respond([
-        'order_id' => $order_id,
-        'status' => 'on_delivery'
+        'success' => true,
+        'data' => [
+            'order_id' => $order_id,
+            'status' => 'on_delivery'
+        ]
     ]);
 }
+
 
 }
